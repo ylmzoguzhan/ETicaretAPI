da2dbcd5a8e9f9669aa5af1774f9dd53ad34ee02
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-08-30 12:28:46 +0300

    Veritabanı oluşturuldu

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETicaretAPIDbCoxtext>(options => options.UseSqlServer(Configuration.ConnectionString));
+        }
+    }
+}
